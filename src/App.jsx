d4fcb35bdd91a16bd045ae395e961b0ca2bf3406
Your Name <chanrhamni>
2023-02-03 23:08:56 +0530

    update all file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import News from './components/News'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>SMS SAREE </h1>
+    </div>
+  )
+}
+
+export default App
