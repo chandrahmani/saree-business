915905a1cab8a10d695d7b0efd2d0fe4c9d62621
Your Name <chanrhamni>
2023-02-08 11:42:37 +0530

    add input fild

--- src/view/signup/Signup.jsx
+++ src/view/signup/Signup.jsx
@@ -21,6 +21,9 @@ const Signup = () => {
 
                     <input type="email" placeholder="enter your email" {...register('email', { required: true, maxLength: 12 })} />
                     {errors.email && errors.email.type === "maxLength" && <span>Ples Enter 12 Letter</span>}
+
+                    <input type="password" placeholder="enter your password" {...register("password", { required: true, maxLength: 10 })} />
+                    {errors.password && errors.password.type === "maxLength" && <p>Please Enter 10 <Letter></Letter></p>}
                     <button type="submit" className="bttn">Submit</button>
                 </form>
             </section>
