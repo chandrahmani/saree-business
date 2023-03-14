51f0296b5d684ebe953f72935e645a014c080b72
Chand Rahmani <chandrahmani254@gmail.com>
2023-03-14 17:04:41 +0530

    update file

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,69 +1,62 @@
-import axios from "axios"
-import { useEffect, useMemo, useState } from "react"
-import ClipLoader from "react-spinners/ClipLoader"
+// import axios from "axios";
+// import { useEffect, useMemo, useState } from "react";
+// import ClipLoader from "react-spinners/ClipLoader";
 
-const Product = () => {
+// const Product = () => {
+//   const [datas, setDatas] = useState([]);
+//   const [loading, setLoding] = useState(false);
 
-    const [datas, setDatas] = useState([])
-    const [loading, setLoding] = useState(false)
+//   const getData = useMemo(() => {
+//     async () => {
+//       try {
+//         const { data: result } = await axios.get("data.json");
 
-    const getData = useMemo(() => {
-        async () => {
-            try {
-                const { data: result } = await axios.get("data.json")
+//         console.log(result);
+//         setDatas(result);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//   }, []);
 
-                console.log(result)
-                setDatas(result)
+//   useEffect(() => {
+//     setLoding(true);
 
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    }, [])
+//     setTimeout(() => {
+//       setLoding(false);
+//     }, 3000);
+//   }, [getData()]);
 
+//   return (
+//     <>
+//       {loading ? (
+//         <ClipLoader
+//           color={"#fff"}
+//           loading={loading}
+//           size={100}
+//           aria-label="Loading Spinner"
+//           data-testid="loader"
+//           className="loder"
+//         />
+//       ) : (
+//         <div>
+//           {datas.map((item, index) => (
+//             <div key={index} className="grid-container">
+//               <div className="grid-item">
+//                 <img src={item.url} />
 
+//                 <div className="info-wrps">
+//                   <h2>{item.date}</h2>
+//                   <h3>{item.silk}</h3>
+//                   <h4>{item.price}</h4>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </>
+//   );
+// };
 
-    useEffect(() => {
-        setLoding(true)
-
-        setTimeout(() => {
-            setLoding(false)
-        }, 3000)
-    }, [getData()])
-
-    return (
-        <>
-            {
-                loading ?
-                    <ClipLoader
-                        color={"#fff"}
-                        loading={loading}
-                        size={100}
-                        aria-label="Loading Spinner"
-                        data-testid="loader"
-                        className='loder'
-                    />
-                    :
-
-                    <div>
-                        {datas.map((item, index) =>
-                            <div key={index} className="grid-container">
-                                <div className='grid-item'>
-                                    <img src={item.url} />
-
-                                    <div className='info-wrps'>
-                                        <h2>{item.date}</h2>
-                                        <h3>{item.silk}</h3>
-                                        <h4>{item.price}</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-            }
-
-        </>
-    )
-}
-
-export default Product;
+// export default Product;
