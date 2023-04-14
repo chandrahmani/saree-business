883a93b1241152b8a24b70a772acdc207ade1b92
chandrahmani <chandrahmani254@gmail.com>
2023-04-14 11:52:26 +0530

    updated Product file fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
 import Navbar from "./components/core/navbar/Navbar";
-// import Product from "./components/product/Product";
+import Product from "./components/product/Product";
 import Signup from "./view/signup/Signup";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="signup" element={<Signup />} />
-          {/* <Route path='products' element={<Product />} /> */}
+          <Route path="products" element={<Product />} />
           <Route />
         </Routes>
       </BrowserRouter>
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,62 +1,62 @@
-// import axios from "axios";
-// import { useEffect, useMemo, useState } from "react";
-// import ClipLoader from "react-spinners/ClipLoader";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
-// const Product = () => {
-//   const [datas, setDatas] = useState([]);
-//   const [loading, setLoding] = useState(false);
+const Product = () => {
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoding] = useState(false);
 
-//   const getData = useMemo(() => {
-//     async () => {
-//       try {
-//         const { data: result } = await axios.get("data.json");
+  const getData = async () => {
+    try {
+      const { data: result } = await axios.get("data.json");
 
-//         console.log(result);
-//         setDatas(result);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//   }, []);
+      console.log(result);
+      setDatas(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-//   useEffect(() => {
-//     setLoding(true);
+  useEffect(() => {
+    setLoding(true);
 
-//     setTimeout(() => {
-//       setLoding(false);
-//     }, 3000);
-//   }, [getData()]);
+    getData();
 
-//   return (
-//     <>
-//       {loading ? (
-//         <ClipLoader
-//           color={"#fff"}
-//           loading={loading}
-//           size={100}
-//           aria-label="Loading Spinner"
-//           data-testid="loader"
-//           className="loder"
-//         />
-//       ) : (
-//         <div>
-//           {datas.map((item, index) => (
-//             <div key={index} className="grid-container">
-//               <div className="grid-item">
-//                 <img src={item.url} />
+    setTimeout(() => {
+      setLoding(false);
+    }, 3000);
+  }, []);
 
-//                 <div className="info-wrps">
-//                   <h2>{item.date}</h2>
-//                   <h3>{item.silk}</h3>
-//                   <h4>{item.price}</h4>
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </>
-//   );
-// };
+  return (
+    <>
+      {loading ? (
+        <ClipLoader
+          color={"#fff"}
+          loading={loading}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          className="loder"
+        />
+      ) : (
+        <div>
+          {datas.map((item, index) => (
+            <div key={index} className="grid-container">
+              <div className="grid-item">
+                <img src={item.url} />
 
-// export default Product;
+                <div className="info-wrps">
+                  <h2>{item.date}</h2>
+                  <h3>{item.silk}</h3>
+                  <h4>{item.price}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Product;
